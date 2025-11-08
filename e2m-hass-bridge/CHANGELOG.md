5c2a95616ab53a9cdfec39e75eebf2cb934810b9
nana4rider <register.develop@nana4.net>
2025-11-08 15:31:24 +0900

    Initial commit

---
+++ e2m-hass-bridge/CHANGELOG.md
@@ -0,0 +1,3 @@
+## v1.14.0-alpha
+
+- Updated e2m-hass-bridge to version [`v1.14.0`](https://github.com/nana4rider/e2m-hass-bridge/releases/tag/v1.14.0)
---
+++ e2m-hass-bridge/entrypoint.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bashio
+
+export PORT=3000
+
+if ! bashio::config.has_value 'mqtt.broker' && bashio::services.available "mqtt"; then
+  bashio::log.info "MQTT service found, fetching credentials ..."
+  if bashio::var.true "$(bashio::services mqtt "ssl")"; then
+    MQTT_SCHEME="mqtts://";
+  else
+    MQTT_SCHEME="mqtt://";
+  fi
+  MQTT_HOST=$(bashio::services mqtt "host")
+  MQTT_PORT=$(bashio::services mqtt "port")
+  export MQTT_BROKER="${MQTT_SCHEME}${MQTT_HOST}:${MQTT_PORT}";
+  export MQTT_USERNAME=$(bashio::services mqtt "username")
+  export MQTT_PASSWORD=$(bashio::services mqtt "password")
+fi
+
+if bashio::config.has_value 'mqtt.broker'; then
+  export MQTT_BROKER=$(bashio::config "mqtt.broker")
+fi
+if bashio::config.has_value 'mqtt.username'; then
+  export MQTT_USERNAME=$(bashio::config "mqtt.username")
+fi
+if bashio::config.has_value 'mqtt.password'; then
+  export MQTT_PASSWORD=$(bashio::config "mqtt.password")
+fi
+if bashio::config.has_value 'ha_discovery_prefix'; then
+  export HA_DISCOVERY_PREFIX=$(bashio::config "ha_discovery_prefix")
+fi
+if bashio::config.has_value 'log_level'; then
+  export LOG_LEVEL=$(bashio::config "log_level")
+fi
+if bashio::config.has_value 'echonetlite2mqtt_base_topic'; then
+  export ECHONETLITE2MQTT_BASE_TOPIC=$(bashio::config "echonetlite2mqtt_base_topic")
+fi
+if bashio::config.has_value 'description_language'; then
+  export DESCRIPTION_LANGUAGE=$(bashio::config "description_language")
+fi
+if bashio::config.has_value 'auto_request_interval'; then
+  export AUTO_REQUEST_INTERVAL=$(bashio::config "auto_request_interval")
+fi
+
+exec node dist/index
